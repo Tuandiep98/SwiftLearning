cd37778daf4ba360adb46605a49445ef39b3a020
tuandiep <tuandiep98usa@gmail.com>
2022-03-19 15:58:41 +0700

    initial swift code

---
+++ SwiftLearning/SwiftLearning/SwiftLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningApp.swift
+//  SwiftLearning
+//
+//  Created by Tuấn Điệp on 19/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
