76e540d9f619ecb45fa17511f53891e1dd642d73
tuandiep <tuandiep98usa@gmail.com>
2022-03-19 23:53:30 +0700

    style changes

--- SwiftLearning/SwiftLearning/screens/home/struct/ProfileView.swift
+++ SwiftLearning/SwiftLearning/screens/home/struct/ProfileView.swift
@@ -63,40 +63,54 @@ struct ProfileDetailView: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 100, alignment: .center)
                 .clipShape(Circle())
-                .padding()
                 VStack(alignment: .leading, spacing: 20){
                     Text("Michel Reed").font(.headline)
                     Text("Technical leader at NASA").font(.subheadline)
                 }
                 .padding()
-            }.frame(alignment: .leading)
+            }.padding()
             Divider()
             HStack{
-                VStack(spacing: 5){
-                    Text("Posts").font(.subheadline)
-                    Text("1.3k").font(.headline)
-                }.padding()
+                Group{
+                    Spacer()
+                    VStack(spacing: 5){
+                        Text("Posts").font(.subheadline)
+                        Text("1.3k").font(.headline)
+                    }.padding()
+                    Spacer()
+                }
                 Divider()
-                VStack(spacing: 5){
-                    Text("Likes").font(.subheadline)
-                    Text("4.5k").font(.headline)
-                }.padding()
+                Group{
+                    Spacer()
+                    VStack(spacing: 5){
+                        Text("Likes").font(.subheadline)
+                        Text("4.5k").font(.headline)
+                    }.padding()
+                    Spacer()
+                }
                 Divider()
-                VStack(spacing: 5){
-                    Text("Followers").font(.subheadline)
-                    Text("20").font(.headline)
-                }.padding()
+                Group{
+                    Spacer()
+                    VStack(spacing: 5){
+                        Text("Followers").font(.subheadline)
+                        Text("20").font(.headline)
+                    }.padding()
+                    Spacer()
+                }
             }.frame(height: 60, alignment: .center)
             Divider()
             VStack(alignment: .leading){
-                Text("Funny videos daily is here..").padding()
+                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry.").font(.subheadline).padding()
                 HStack{
                     Button(action: followFunc){
                         Label("Follow", systemImage: "heart.circle")
-                    }.frame(width: 200).buttonStyle(RoundedRectangleButtonStyle())
+                    }
+                    .frame(width: 200)
+                    .buttonStyle(RoundedRectangleButtonStyle())
                     Button(action: chatFunc){
                         Text("Chat")
-                    }.buttonStyle(RoundedRectangleButtonStyle(labelColor: .black, backgroundColor: Color.init(hex: "#ecf0f1")))
+                    }
+                    .buttonStyle(RoundedRectangleButtonStyle(labelColor: .black, backgroundColor: Color.init(hex: "#ecf0f1")))
                 }.padding()
             }
             Divider()
